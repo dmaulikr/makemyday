e38626afaa98c833480ea4c1cb17ba80b1fe67d3
misslyli <only_lyn@hotmail.com>
2016-03-25 16:09:58 +0100

    Xcode project creation

---
+++ Code/makemyday/makemydayTests/makemydayTests.swift
@@ -0,0 +1,36 @@
+//
+//  makemydayTests.swift
+//  makemydayTests
+//
+//  Created by Lynn Olame on 25/03/2016.
+//  Copyright © 2016 Lynn Olame. All rights reserved.
+//
+
+import XCTest
+@testable import makemyday
+
+class makemydayTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
